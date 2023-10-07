46d1cafee293b2a8794825166f0c9c5bc9460369
Daniel Muñoz <88912913+WinerMaster@users.noreply.github.com>
2023-10-07 01:37:20 -0300

    Update register.php

--- php/register.php
+++ php/register.php
@@ -11,15 +11,23 @@ $values = (array)json_decode($post);
 if(!!$values['username'] && !!$values['password']) {
     $user = filter_var($values['username'], FILTER_SANITIZE_SPECIAL_CHARS);
     $pass = $values['password'];
-    $pass = password_hash($pass, PASSWORD_DEFAULT);
-    $sql = $conn->query("INSERT INTO users(username, pass) VALUES('{$user}', '{$pass}')");
     
-    if($sql) {
-        $_SESSION['name'] = $user;
-        echo "success";
+    $sql = $conn->query("SELECT username FROM users WHERE username = '{$user}'");
+    
+    if($sql->num_rows > 0) {
+        echo "El usuario ya existe";
     } else {
-        echo "Error al registrarse.";
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
+        $sql = $conn->query("INSERT INTO users(username, pass) VALUES('{$user}', '{$pass}')");
+        
+        if($sql) {
+            $_SESSION['name'] = $user;
+            echo "success";
+        } else {
+            echo "Error al registrarse.";
+        }
     }
+    
 } else {
     echo 'Campos sin completar.';
 }
